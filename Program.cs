f89ad1c4c95d61db5921ee47f169ab7d95d9525a
Freddie-Read <57951805+Freddie-Read@users.noreply.github.com>
2022-11-21 00:49:23 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string[] noises = { "meow", "hiss", "mew", "purr", "grr", "sss", "yowl", "mmmmm" };
+string[] punc = { ".", "!", "...", "?" };
+
+Random random = new Random();
+int sentenceLength = random.Next(7, 23);
+int paragraphLength = random.Next(5, 9);
+
+string paragraph = "";
+
+for (int i = 0; i < paragraphLength; i++)
+{
+    string sentence = "";
+    for (int j = 0; j < sentenceLength; j++)
+    {
+        int noiseIndex = random.Next(0, 7);
+
+        if (j == 0)
+        {
+            sentence += char.ToUpper(noises[noiseIndex][0]) + noises[noiseIndex].Substring(1) + " ";
+        }
+        else
+        {
+            sentence += noises[noiseIndex] + " ";
+        }
+    }
+    sentenceLength = random.Next(13, 23);
+    paragraph += sentence.Trim() + punc[random.Next(0, 3)] + " ";
+}
+
+Console.WriteLine(paragraph);
---
+++ README.md
@@ -0,0 +1,3 @@
+# Cat Talking
+Randomly generates a paragraph of cat speak.
+Based on: https://sebastiancarlos.medium.com/how-i-quit-my-programmer-job-to-become-a-chicken-b733c20680b1
